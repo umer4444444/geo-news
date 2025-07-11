73e297f15c695593f44288edfbb2531f9520a0ed
awais9642 <awaisshakir679@gmail.com>
2025-07-11 18:23:38 +0500

    push

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -6,6 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type Article = {
+  
   title: string;
   source: { name: string };
   urlToImage?: string;
